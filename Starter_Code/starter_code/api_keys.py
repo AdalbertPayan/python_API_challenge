6986b473e041414dfcc65a6767eda984272b567f
AdalbertPayan <adalbertpayan@gmail.com>
2023-08-09 18:48:29 -0700

    update

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f7d7e243be344ade617ba29d1292d46d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "9c2d0f2c95104392bfbf13741c9ecb28"
